a0cece81f57c030be7e0b5440254978089645acd
Farzan Khan <farzankhan61r@gmail.com>
2025-12-27 01:22:27 +0530

    fix: tailwind css in prod

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 set -o errexit
+
+curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
+chmod +x tailwindcss-linux-x64
+
+# Build Tailwind CSS
+./tailwindcss-linux-x64 -i ./static/input.css -o ./static/tailwind.css --minify
+
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
